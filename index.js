c0353687bace7c6b53b9d5bc65bb049470b0ae93
christian ferrer <cferrercava@gmail.com>
2021-09-21 08:50:22 -0500

    add renwe connection to server

--- index.js
+++ index.js
@@ -1,82 +1,83 @@
 // let url = 'ws://localhost:3000' 
 let url = 'wss://aqueous-river-42785.herokuapp.com';
 
- 
 
-let client = new WebSocket(url); 
+
+let client = new WebSocket(url);
 
 client.addEventListener("open", () => {
   console.log(`[websockets] Connected to ${url}`);
+  renewConnection()
 });
 client.addEventListener("message", (data) => {
-  
-      var _data = JSON.parse(data.data)
-      console.log(_data);
-      if (_data.event === 'connect') {
-        saveID(_data.id)
-      }
-      if (_data.event === 'result_getClients') {
-        if (_data.data !== null || _data.data !== undefined) {
-          updateList(_data.data)
-        }
-      }
+
+  var _data = JSON.parse(data.data)
+  console.log(_data);
+  if (_data.event === 'connect') {
+    saveID(_data.id)
+  }
+  if (_data.event === 'result_getClients') {
+    if (_data.data !== null || _data.data !== undefined) {
+      updateList(_data.data)
+    }
+  }
 });
 
 
 
 
 //save ID
-const saveID = (id)=>{
-  localStorage.setItem('ID',id)
+const saveID = (id) => {
+  localStorage.setItem('ID', id)
 }
-const getID = ()=>{
- return localStorage.getItem('ID')
+const getID = () => {
+  return localStorage.getItem('ID')
 }
 
 //send unique ID
-const sendUniqueID = ()=>{
+const sendUniqueID = () => {
   var value = document.getElementById('txt_ID').value
   var message = {
     event: 'setUniqueID',
-    data : value,
-    id:getID()
+    data: value,
+    id: getID()
   }
   client.send(JSON.stringify(message))
 }
 
 
 //send travel
-const sendMessage=()=>{
-    var txt_data = document.getElementById('txt_data').value
-    var txt_cliendID = document.getElementById('txt_cliendID').value
-    var _data = {
-        event: 'sendToMessageToParticularClient',
-        client:txt_cliendID,
-        data: {
-           ruta: txt_data ?? '---',
-           time:'15:00',
-        }
-    }
-    client.send(JSON.stringify(_data))
-}
-
-const getConnections=()=>{
+const sendMessage = () => {
+  var txt_data = document.getElementById('txt_data').value
   var txt_cliendID = document.getElementById('txt_cliendID').value
   var _data = {
-      event: 'getClients',
-      client:txt_cliendID, 
+    event: 'sendToMessageToParticularClient',
+    client: txt_cliendID,
+    data: {
+      ruta: txt_data ?? '---',
+      time: '15:00',
+    }
+  }
+  client.send(JSON.stringify(_data))
+}
+
+const getConnections = () => {
+  var txt_cliendID = document.getElementById('txt_cliendID').value
+  var _data = {
+    event: 'getClients',
+    client: txt_cliendID,
   }
   client.send(JSON.stringify(_data))
 }
 
 
-const updateList = (data)=>{
-  var ul = document.getElementById('list_connections') ;
-  ul.innerHTML=''
-  data.forEach((value)=>{
-    ul.innerHTML += '<li>'+ value.uniqueID+'</li>'
+const updateList = (data) => {
+  var ul = document.getElementById('list_connections');
+  ul.innerHTML = ''
+  data.forEach((value) => {
+    ul.innerHTML += '<li>' + value.uniqueID + '</li>'
   })
-  
+
 
 }
 
@@ -84,3 +85,14 @@ client.addEventListener("close", () => {
   console.log(`[websockets] Disconnected from ${url}`);
   client = null;
 });
+
+const renewConnection = () => {
+  var _data = {
+    event: 'renewConnection',
+    id: getID(),
+    data: {}
+  }
+  setInterval(() => {
+    client.send(JSON.stringify(_data))
+  }, 50000);
+}
